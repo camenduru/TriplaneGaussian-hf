0903bb49e26a1d90b0eaf6317808bff2638cb620
zouzx <njzzx123@163.com>
2024-01-08 22:08:23 +0800

    update

--- app.py
+++ app.py
@@ -143,9 +143,9 @@ def launch(port):
                 )
             
             with gr.Column(scale=1):
-                with gr.Row(variant='panel'):
-                    seg_image = gr.Image(value=None, type="filepath", height=256, width=256, image_mode="RGBA", label="Segmented Image", interactive=False)
-                    output_video = gr.Video(value=None, label="Rendered Video", height=256, width=256, autoplay=True)
+                # with gr.Row(variant='panel'):
+                seg_image = gr.Image(value=None, type="filepath", image_mode="RGBA", label="Segmented Image", interactive=False)
+                output_video = gr.Video(value=None, label="Rendered Video", autoplay=True)
                 output_3dgs = Model3DGS(value=None, label="3D Model")
         
         img_run_btn.click(
